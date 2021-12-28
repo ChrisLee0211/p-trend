0281d77ae2d6ff4db6ddc12af189d834bfc5b5f3
李铭辉 <1018596741@qq.com>
2021-12-28 22:13:19 +0800

    feat: 新增dev模式用于调试

---
+++ sample/index.ts
@@ -0,0 +1,40 @@
+import { ScanerCtr } from "../src/core/scaner";
+import { PraserCtr } from '../src/core/praser';
+import { Server } from '../src/server';
+import { resolveExternals, resolvePackage } from "../src/core/helper/resolvePackage";
+import { log } from '../src/utils/log';
+
+const defaultConfig = {
+    entry:'src/',
+    port:8080,
+    externals:{},
+    alias: undefined,
+};
+
+const test = async () => {
+    try{
+        /** 解析外部依赖 */
+        const externals:string[] = [];
+        if(defaultConfig?.externals) {
+            externals.push(...resolveExternals(defaultConfig.externals));
+        }
+        const npmDependency:string[] = [];
+        try{
+            const packageDeps = await resolvePackage();
+            npmDependency.push(...packageDeps);
+        }catch(e) {
+            console.error(e);
+        }
+        const praser = new PraserCtr(defaultConfig.alias, npmDependency, externals);
+        const scaner = new ScanerCtr(defaultConfig.entry);
+        await scaner.scan(praser.parseDependency, praser);
+        await scaner.buildFileTree();
+        scaner.diff();
+        new Server(scaner,defaultConfig.entry, defaultConfig.port);
+        log(`完成扫描，请打开地址：http://localhost:${defaultConfig.port}/p-trend`,'success');
+    }catch(e){
+        console.error(e);
+    }
+};
+
+test();
