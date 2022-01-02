04368d9a63b520f64c177e5ab75d637e56668898
李铭辉 <1018596741@qq.com>
2022-01-02 19:34:35 +0800

    fix: 修复更改目录节点时丢失依赖标记问题

---
+++ client/src/page/Graph/hook/useGraph.ts
@@ -0,0 +1,95 @@
+import G6, { Item, NodeConfig, TreeGraph, TreeGraphData } from '@antv/g6';
+import {nextTick, Ref, ref} from 'vue';
+
+export const useGraph = (containerRef: Ref<HTMLDivElement | null>) => {
+    const graphIns = ref<TreeGraph | null>(null);
+    const isFirstRender = ref(true);
+    const loading = ref(true);
+    const initGraph = (data: TreeGraphData, plugins:any[]) => {
+        if(data&& isFirstRender.value) {
+            nextTick(() => {
+                graphIns.value = new G6.TreeGraph({
+                  container: containerRef.value as HTMLDivElement,
+                  modes: {
+                    default: [
+                      {
+                        type: "collapse-expand",
+                        onChange: function onChange(item, collapsed) {
+                          const data = (item as Item).getModel();
+                          data.collapsed = collapsed;
+                          return true;
+                        },
+                      },
+                      "drag-canvas",
+                      "zoom-canvas",
+                    ],
+                  },
+                  defaultNode: {
+                    size: 26,
+                    anchorPoints: [
+                      [0, 0.5],
+                      [1, 0.5],
+                    ],
+                  },
+                  defaultEdge: {
+                    type: "cubic-horizontal",
+                  },
+                  plugins,
+                  layout: {
+                    type: "compactBox",
+                    direction: "LR",
+                    getId: function getId(d: any) {
+                      return d.id;
+                    },
+                    getHeight: function getHeight() {
+                      return 20;
+                    },
+                    getWidth: function getWidth() {
+                      return 20;
+                    },
+                    getVGap: function getVGap() {
+                      return 10;
+                    },
+                    getHGap: function getHGap() {
+                      return 100;
+                    },
+                  },
+                });
+                graphIns.value.node((node): Partial<NodeConfig> => {
+                  let baseConfig: Partial<NodeConfig> = {
+                    label: node.name as string,
+                    labelCfg: {
+                      offset: 10,
+                      position:
+                        node.children && node.children.length > 0 ? "left" : "right",
+                    },
+                  };
+                  if (node?.isDep) {
+                    baseConfig = {
+                      ...baseConfig,
+                      style: {
+                        shadowBlur: 10,
+                        fill: "#1a414e",
+                        stroke: "black",
+                      },
+                    };
+                  }
+                  return baseConfig;
+                });
+                graphIns.value.on("afterlayout", (evt) => {
+                  isFirstRender.value = false;
+                  loading.value = false;
+                });
+                graphIns.value.data(data as unknown as TreeGraphData);
+                graphIns.value.render();
+                graphIns.value.fitView();
+              });
+        }
+    };
+
+    return {
+        graphIns,
+        initGraph,
+        loading
+    };
+};
