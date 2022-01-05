8668fcab628be2976bde74de003913fd36105462
李铭辉 <1018596741@qq.com>
2022-01-05 21:37:29 +0800

    feat:swc增加解析import规则

--- src/core/praser/swcParser.ts
+++ src/core/praser/swcParser.ts
@@ -2,7 +2,7 @@
  * 试验性尝试swc代替babel作为代码解析器，
  * 对于parser主体api不做任何更改，保证和collectImportNodes 方法一样的类型和输入输出作为替换方案
  */
-import { Module } from "@swc/core/types";
+import { Module,Argument } from "@swc/core/types";
 import { Visitor } from "@swc/core/Visitor.js";
 
 type CollectFnType = (path:string) => void
@@ -14,6 +14,17 @@ type CollectFnType = (path:string) => void
         if (cb) {
             this.collectFn = cb;
         }
+        this.getFirstArgsValue = this.getFirstArgsValue.bind(this);
+    }
+    getFirstArgsValue(args?:Array<Argument>):string {
+        let path = '';
+        if (args && args.length) {
+            const firstArg = args[0];
+            if (firstArg.expression.type === 'StringLiteral') {
+                 path = firstArg.expression.value;
+            }
+        }
+        return path;
     }
      visitModule(m: Module): Module {
          const importContent = m.body;
@@ -30,13 +41,15 @@ type CollectFnType = (path:string) => void
                      const target = declareSouce[0];
                      if (target.type === 'VariableDeclarator' && target.init?.type === 'CallExpression') {
                          const args = target.init.arguments;
-                         if (args.length) {
-                             const firstArg = args[0];
-                             if (firstArg.expression.type === 'StringLiteral') {
-                                  path = firstArg.expression.value;
-                             }
-                         }
+                         path = this.getFirstArgsValue(args);
                      }
+                 }
+             }
+             if(importNode.type === 'ExpressionStatement') {
+                 const expression = importNode.expression;
+                 if (expression.type ==='CallExpression') {
+                     const args = expression.arguments;
+                     path = this.getFirstArgsValue(args);
                  }
              }
              if(this.collectFn && path.length > 0) {
