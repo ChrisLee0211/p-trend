b8bc31539d86334e904c5012d16f053a9f44beb6
李铭辉 <1018596741@qq.com>
2022-02-12 16:01:50 +0800

    feat: 完成第三方依赖列表展示组件

--- client/src/page/Pkg/index.vue
+++ client/src/page/Pkg/index.vue
@@ -10,17 +10,48 @@
         <div id="word-cloud"></div>
       </section>
       <!-- 依赖列表 -->
-      <section class="content-list"></section>
+      <section class="content-list">
+        <header>
+          <div class="total">
+              共 
+              <n-number-animation ref="numberAnimationInstRef" :from="0" :to="chartData.length" />
+              个第三方依赖</div>
+          <dir class="sort">
+            <n-radio-group v-model:value="sort" name="sort">
+              <n-space>
+                <n-radio value="down"> 倒序 </n-radio>
+                <n-radio value="up"> 升序 </n-radio>
+              </n-space>
+            </n-radio-group>
+          </dir>
+        </header>
+        <n-collapse>
+          <n-collapse-item
+            v-for="item in chartData"
+            :key="item.name"
+            :name="item.name"
+            :title="`${item.name}`"
+          >
+            <template #header-extra>共被引用了{{ item.count }}次</template>
+            <li
+              class="reference-path"
+              v-for="path in item.reference"
+              :key="path"
+            >
+              <div class="reference-path-name">路径:{{ path }}</div>
+              <n-button text quaternary type="primary">预览</n-button>
+            </li>
+          </n-collapse-item>
+        </n-collapse>
+      </section>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { useFetch } from "@vueuse/core";
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, watchEffect } from "vue";
 import { WordCloud } from "@antv/g2plot";
-import {
-  NSpin,
-} from "naive-ui";
+import { NSpin, NCollapse, NCollapseItem, NButton, NRadioGroup,NSpace,NRadio, NNumberAnimation } from "naive-ui";
 
 interface NpmListItem {
   count: number;
@@ -30,17 +61,26 @@ interface NpmListItem {
 
 export default defineComponent({
   name: "package",
-  components:{NSpin},
+  components: {
+    NSpin,
+    NCollapseItem,
+    NCollapse,
+    NButton,
+    NRadioGroup,
+    NSpace,
+    NRadio,
+    NNumberAnimation
+  },
   setup() {
     const chartIns = ref<WordCloud | null>(null);
-    const chartData = ref<NpmListItem[]>()
+    const chartData = ref<NpmListItem[]>([]);
     const chartLoading = ref(true);
-    const { onFetchResponse, data } = useFetch<{deps:NpmListItem[]}>(
+    const { onFetchResponse, data } = useFetch<{ deps: NpmListItem[] }>(
       `http://localhost:${window.preloadState.port}/pkg`
-    ).json<{deps:NpmListItem[]}>();
+    ).json<{ deps: NpmListItem[] }>();
     onFetchResponse((res) => {
-        console.log('data ===>', data.value)
       if (data.value !== null) {
+        chartData.value = data.value.deps;
         chartIns.value = new WordCloud("word-cloud", {
           data: data.value.deps,
           wordField: "name",
@@ -97,66 +137,111 @@ export default defineComponent({
           },
         });
         chartIns.value.render();
-        chartLoading.value = false
+        chartLoading.value = false;
+      }
+    });
+
+    const sort = ref<"up" | "down">("down");
+    watchEffect(() => {
+      if (sort.value === "up") {
+        chartData.value = chartData.value.sort((a, b) => {
+          return a.count - b.count;
+        });
+      } else {
+        chartData.value = chartData.value.sort((a, b) => {
+          return b.count - a.count;
+        });
       }
     });
 
     return {
       chartLoading,
+      chartData,
+      sort,
     };
   },
 });
 </script>
 <style lang="scss">
 .pkg {
-    width:100%;
-    height:100%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  box-sizing: border-box;
+  .header {
+    width: 100%;
+    height: 50px;
     display: flex;
-    flex-direction: column;
-    padding: 10px;
-    .header {
-        width:100%;
-        height: 50px;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 17px 1px #8080802e;
+  }
+  .content {
+    width: 100%;
+    flex: 1;
+    min-height: 200px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &-chart {
+      width: 65%;
+      height: 100%;
+      position: relative;
+      border-radius: 5px;
+      box-shadow: 0px 0px 17px 1px #8080802e;
+      .loading-wrapper {
+        position: absolute;
+        top: 0px;
+        bottom: 0px;
+        left: 0px;
+        right: 0px;
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 30px;
-        margin-bottom: 10px;
-        border-radius: 5px;
-        box-shadow: 0px 0px 4px 0px #8080805e;
-    }
-    .content{
+      }
+      #word-cloud {
         width: 100%;
-        flex: 1;
-        min-height: 200px;
+        height: 100%;
+      }
+    }
+    &-list {
+      width: 35%;
+      height: 100%;
+      box-shadow: 0px 0px 17px 1px #8080802e;
+      border-radius: 5px;
+      overflow-y: scroll;
+      margin-left: 10px;
+      /* padding: 16px 0px; */
+      box-sizing: border-box;
+      & header {
+        padding: 10px;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        &-chart {
-            width: 65%;
-            height: 100%;
-            position: relative;
-            border-radius: 5px;
-            box-shadow: 0px 0px 4px 0px #8080805e;
-            .loading-wrapper {
-                position: absolute;
-                top: 0px;
-                bottom: 0px;
-                left: 0px;
-                right: 0px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            };
-            #word-cloud {
-                width: 100%;
-                height: 100%;
-            }
+        .total {
+          font-size: 20px;
         }
-        &-list {
-            width: 35%;
-            height: 100%;
+        .sort {
         }
+      }
+      .n-collapse-item {
+        content-visibility: auto;
+      }
+      .reference-path {
+        padding: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        &-name {
+          margin-right: 10px;
+        }
+      }
     }
+  }
 }
 </style>
