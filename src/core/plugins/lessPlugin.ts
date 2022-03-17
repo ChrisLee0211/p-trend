a1ffa81d6999598af6bf1d0f088f738147431075
李铭辉 <1018596741@qq.com>
2022-03-17 23:11:32 +0800

    feat: 增加postcss插件机制扫描less文件

--- src/core/plugins/lessPlugin.ts
+++ src/core/plugins/lessPlugin.ts
@@ -1,16 +1,10 @@
 import { ParserPlugin } from "@/types/global";
-import * as less from 'less';
 
 const lessImportParser = (code:string):Promise<string[]> => {
     const result:string[] = [];
     return new Promise((rs,rj) => {
         try{
-            less.render(code,(err,output) => {
-                if(output?.imports && output?.imports.length) {
-                    result.push(...output.imports);
-                }
-                rs(result);
-            });
+            // 使用postcss尝试解析less
         }catch(e) {
             rj(new Error('Fail to parse less file'));
         }
@@ -18,7 +12,7 @@ const lessImportParser = (code:string):Promise<string[]> => {
 };
 
 /**
- * 用于解析js、ts类型文件的引用（包含jsx和tsx）
+ * 用于解析less类型文件的引用(包含url、@import)
  */
 export const lessPlugin:ParserPlugin = {
     rule:/\.(less)$/,
