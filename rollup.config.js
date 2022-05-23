74b9bf03fa5f72f4621b369645f02db9b9e48864
李铭辉 <1018596741@qq.com>
2022-05-23 22:57:27 +0800

    chore: add rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,15 @@
-import typescript from "@rollup/plugin-typescript";
-import babel from "rollup-plugin-babel";
-import sourceMaps from "rollup-plugin-sourcemaps";
-import resolve from "@rollup/plugin-node-resolve";
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import json from 'rollup-plugin-json';
-import { eslint } from 'rollup-plugin-eslint';
+import json from '@rollup/plugin-json';
 
-export default {
-  input: "./src/index.ts",
-  plugins: [
-    typescript(),
-    resolve(),
-    commonjs(),
-    json(),
-    eslint({
-      throwOnError: true,
-      throwOnWarning: false,
-      include: ['src/**'],
-      exclude: ['node_modules/**']
-    }),
-    sourceMaps(),
-    // babel({
-    //   exclude:'node_modules/**'
-    // })
-  ],
-  external:['vue'],
-  output: [
-    {
-      format: "es",
-      file: "lib/bundle.esm.js",
-      sourcemap: true
-    }
-  ]
-};
+export default  {
+    input: './src/index.ts',
+    external: ['@swc/core'],
+    output: {
+      dir: 'dist',
+      format: 'cjs',
+      entryFileNames: '[name].cjs.js',
+    },
+    plugins: [resolve(), commonjs(),json(), typescript({module:'ESNext'})],
+  };
